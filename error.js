00dd20df5eed0187621718b117c3601a4f129cbe
Lemito66 <lemito66@gmail.com>
2024-01-16 13:35:06 -0500

    manejo de errores con axios

---
+++ error.js
@@ -0,0 +1,18 @@
+async function main() {
+  try {
+    const res = await axios.post("https://reqres.in/api/login", {
+      email: "peter@klaven",
+    });
+    console.log(res);
+  } catch (error) {
+    if (error.response.status === 404) {
+      console.log("No se encontró peticion");
+    }
+    if (error.response.status === 400) {
+      console.log("Datos erroneos");
+      console.log(error.response.data.error);
+    }
+  }
+}
+
+main();
