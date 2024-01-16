57db786373394c611012d93a5ca053c9146ccdfd
Lemito66 <lemito66@gmail.com>
2024-01-16 13:52:17 -0500

    timeout con axios

---
+++ timeout.js
@@ -0,0 +1,24 @@
+async function main() {
+    const axiosInstance = axios.create({
+        baseURL: "https://jsonplaceholder.typicode.com",
+        timeout: 20,
+    });
+
+    try {
+      const res = await axiosInstance.get("/posts/1", {
+        email: "peter@klaven",
+      });
+      console.log(res);
+    } catch (error) {
+      if (error.response.status === 404) {
+        console.log("No se encontró peticion");
+      }
+      if (error.response.status === 400) {
+        console.log("Datos erroneos");
+        console.log(error.response.data.error);
+      }
+    }
+  }
+  
+  main();
+  
